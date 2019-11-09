fd37c10620386dfa32caf6f7438fec7a832cb2b5
Jin Xue <csjinxue@outlook.com>
2019-11-10 00:28:33 +0800

    Add error handling in FFI

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -1,9 +1,62 @@
 extern crate libc;
 
-use crate::{catalog::Schema, storage::TablePtr, DBConfig, DB, OID};
+use crate::{catalog::Schema, storage::TablePtr, DBConfig, Error, DB, OID};
 
-use libc::c_char;
-use std::{ffi::CStr, path::PathBuf, sync::Arc};
+use libc::{c_char, c_int};
+use std::{cell::RefCell, ffi::CStr, path::PathBuf, sync::Arc};
+
+// error handling code borrowed from https://michael-f-bryan.github.io/rust-ffi-guide/errors/return_types.html
+thread_local! {
+    static LAST_ERROR: RefCell<Option<Box<Error>>> = RefCell::new(None);
+}
+
+fn update_last_error(err: Error) {
+    LAST_ERROR.with(|prev| {
+        *prev.borrow_mut() = Some(Box::new(err));
+    });
+}
+
+fn take_last_error() -> Option<Box<Error>> {
+    LAST_ERROR.with(|prev| prev.borrow_mut().take())
+}
+
+#[no_mangle]
+pub extern "C" fn sq_last_error_length() -> c_int {
+    LAST_ERROR.with(|prev| match *prev.borrow() {
+        Some(ref err) => err.to_string().len() as c_int + 1,
+        None => 0,
+    })
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn sq_last_error_message(buffer: *mut c_char, length: c_int) -> c_int {
+    if buffer.is_null() {
+        return -1;
+    }
+
+    let last_error = match take_last_error() {
+        Some(err) => err,
+        None => return 0,
+    };
+
+    let error_message = last_error.to_string();
+
+    let buffer = std::slice::from_raw_parts_mut(buffer as *mut u8, length as usize);
+
+    if error_message.len() >= buffer.len() {
+        return -1;
+    }
+
+    std::ptr::copy_nonoverlapping(
+        error_message.as_ptr(),
+        buffer.as_mut_ptr(),
+        error_message.len(),
+    );
+
+    buffer[error_message.len()] = 0;
+
+    error_message.len() as c_int
+}
 
 #[no_mangle]
 pub extern "C" fn sq_create_db(root_path: *const c_char) -> *const DB {
@@ -13,7 +66,14 @@ pub extern "C" fn sq_create_db(root_path: *const c_char) -> *const DB {
     };
     let root_path_str = root_path.to_str().unwrap();
     let config = DBConfig::new().root_path(PathBuf::from(root_path_str));
-    Arc::into_raw(Arc::new(DB::open(&config).unwrap()))
+    let db = match DB::open(&config) {
+        Ok(db) => db,
+        Err(e) => {
+            update_last_error(e);
+            return std::ptr::null();
+        }
+    };
+    Arc::into_raw(Arc::new(db))
 }
 
 #[no_mangle]
@@ -33,7 +93,14 @@ pub extern "C" fn sq_create_table(db: *const DB, db_oid: OID, rel_oid: OID) -> *
         &*db
     };
 
-    let table = db.create_table(db_oid, rel_oid, Schema::new()).unwrap();
+    let table = match db.create_table(db_oid, rel_oid, Schema::new()) {
+        Ok(table) => table,
+        Err(e) => {
+            update_last_error(e);
+            return std::ptr::null();
+        }
+    };
+
     Box::into_raw(Box::new(table))
 }
 
@@ -53,7 +120,7 @@ pub extern "C" fn sq_table_insert_tuple(
     db: *const DB,
     data: *const u8,
     len: u64,
-) {
+) -> c_int {
     let db = unsafe {
         assert!(!db.is_null());
         &*db
@@ -65,5 +132,11 @@ pub extern "C" fn sq_table_insert_tuple(
 
     let tuple = unsafe { std::slice::from_raw_parts(data, len as usize) };
 
-    table.insert_tuple(db, tuple).unwrap();
+    match table.insert_tuple(db, tuple) {
+        Ok(_) => 1,
+        Err(e) => {
+            update_last_error(e);
+            0
+        }
+    }
 }
--- src/result.rs
+++ src/result.rs
@@ -26,12 +26,34 @@ impl From<io::Error> for Error {
 
 impl StdError for Error {
     fn description(&self) -> &str {
-        ""
+        use self::Error::*;
+
+        match *self {
+            FileAccess(ref e)
+            | WrongObjectType(ref e)
+            | DataCorrupted(ref e)
+            | ProgramLimitExceed(ref e)
+            | InvalidState(ref e)
+            | InvalidArgument(ref e) => &*e,
+            Io(ref e) => e.description(),
+            OutOfMemory => "out of memory",
+        }
     }
 }
 
 impl Display for Error {
-    fn fmt(&self, _f: &mut fmt::Formatter<'_>) -> std::result::Result<(), fmt::Error> {
-        Ok(())
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> std::result::Result<(), fmt::Error> {
+        use self::Error::*;
+
+        match *self {
+            FileAccess(ref e) => write!(f, "File access error: {}", e),
+            WrongObjectType(ref e) => write!(f, "Wrong objet type: {}", e),
+            DataCorrupted(ref e) => write!(f, "Data corrupted: {}", e),
+            ProgramLimitExceed(ref e) => write!(f, "Program limit exceed: {}", e),
+            InvalidState(ref e) => write!(f, "Invalid state: {}", e),
+            InvalidArgument(ref e) => write!(f, "Invalid argument: {}", e),
+            Io(ref e) => write!(f, "IO error: {}", e),
+            OutOfMemory => write!(f, "Out of memory."),
+        }
     }
 }
